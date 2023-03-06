2ecb1671544396b226674e7d03650a223e8c5460
n-dett <124851780+n-dett@users.noreply.github.com>
2023-03-05 21:44:35 -0600

    Add first draft of js pseudocode

---
+++ rock-p-s.js
@@ -0,0 +1,18 @@
+// Create function getComputerChoice
+// getComputerChoice will randomly return either 'Rock', 'Paper, or 'Scissors'
+// console.log to ensure this returns the expected output
+
+// Create function playRound to play single round of Rock Paper Scissors
+// playRound should have parameters (playerSelection, computerSelection)
+// Return a string that declares the winner of the round
+// Make playerSelection case-insensitive
+// Test with console.log
+
+// Create new function called game
+// Call playRound inside of game to play a 5 round game using a loop
+// game keeps score and reports a winner or loser at the end
+// Use console.log to display results of each round and winner at the end
+// Use prompt() to get input from the user
+// Re-work previous functions if needed
+// Can change return value to something more useful
+// Can create more 'helper' functions if needed
