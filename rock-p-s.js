67f6bd9cf7ac3bedb5c0bea79010e4330622897e
n-dett <124851780+n-dett@users.noreply.github.com>
2023-03-10 20:36:31 -0600

    Add computer selection and gameplay in js
    Created a function for computer choice and a function for each game outcome

--- rock-p-s.js
+++ rock-p-s.js
@@ -1,12 +1,40 @@
-// Create function getComputerChoice
-// getComputerChoice will randomly return either 'Rock', 'Paper, or 'Scissors'
-// console.log to ensure this returns the expected output
+function getComputerChoice() {
+    let choices = ['Rock','Paper','Scissors']
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice())
+let computerSelection = getComputerChoice()
 
 // Create function playRound to play single round of Rock Paper Scissors
 // playRound should have parameters (playerSelection, computerSelection)
 // Return a string that declares the winner of the round
 // Make playerSelection case-insensitive
 // Test with console.log
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "Rock" && computerSelection === "Paper"){
+    return "You lose! Paper beats rock."
+  }
+  else if (playerSelection === "Paper" && computerSelection === "Rock"){
+    return "You win! Paper beats rock."
+  }
+  else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+    return "You win! Scissors beats paper."
+  }
+  else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+    return "You lose! Scissors beats paper."
+  }
+  else if (playerSelection === "Rock" && computerSelection === "Scissors"){
+    return "You win! Rock beats scissors."
+  }
+  else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+    return "You lose! Rock beats scissors."
+  }
+  else {
+    return "Tie game!"
+  }
+}
 
 // Create new function called game
 // Call playRound inside of game to play a 5 round game using a loop
