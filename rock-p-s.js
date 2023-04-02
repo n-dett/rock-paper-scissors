a99a0103abe2b2db0e1561812a5c03a18ef9cdcd
n-dett <124851780+n-dett@users.noreply.github.com>
2023-04-02 00:07:03 -0500

    Add results div and text

--- rock-p-s.js
+++ rock-p-s.js
@@ -1,9 +1,53 @@
+let results = document.querySelector("#results");
+let computerChoiceText = document.querySelector('#computer-choice-text');
+let playerChoiceText = document.querySelector('#player-choice-text');
+
 function getComputerChoice() {
   let choices = ['rock','paper','scissors']
-  return choices[Math.floor(Math.random() * choices.length)];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+  console.log(computerChoice);
+  return computerChoice;
 }
 
+
+/////////////////////////
+
+// Add event listener to player choice buttons
+const gameBtns = document.querySelectorAll('.gameBtns');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+let buttonClicked = false;
+
+function playerChoice(btn) {
+  rockBtn.addEventListener('click', () => {
+    buttonClicked = true;
+  })
+  paperBtn.addEventListener('click', () => {
+    buttonClicked = true;
+  })
+  scissorsBtn.addEventListener('click', () => {
+    buttonClicked = true;
+  })
+  if(buttonClicked == true){
+    return btn.id;
+  }
+}
+
+gameBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    console.log(playRound(playerChoice(), getComputerChoice()));
+  })
+})
+
+////////////////////////
+
+
 function playRound(playerSelection, computerSelection){
+  computerChoiceText.innerText = `Computer chose ${computerSelection}`;
+  playerChoiceText.innerText = `You chose ${playerSelection}`;
+
   if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'paper' && computerSelection === 'rock') ||
       (playerSelection === 'scissors' && computerSelection === 'paper')){
@@ -25,38 +69,26 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-/////////////////////////
-
-// Add click event listener to buttons
-const gameButtons = document.querySelectorAll('.gameButtons');
-gameButtons.forEach(function(btn){
-  btn.addEventListener('click', clickedButton);
-})
-// Log value of button
-function clickedButton(btn) {
-  console.log(btn.target.id);
-}
-
-////////////////////////
 
 
-function game(){
+
+//function game(){
   //for (let i = 0; i < 5; i++){
     //let playerSelection = prompt("Enter rock, paper, or scissors").toLowerCase(); 
-    const computerSelection = getComputerChoice();
-    console.log(computerSelection);
+    //const computerSelection = getComputerChoice();
+    //console.log(computerSelection);
 
-    console.log(playRound(playerSelection, computerSelection));
-    console.log('Your score: ' + playerScore) 
-    console.log('Computer score: ' + computerScore)
+    //console.log(playRound(playerSelection, computerSelection));
+    //console.log('Your score: ' + playerScore) 
+    //console.log('Computer score: ' + computerScore)
   //}
   
-  if (playerScore > computerScore){
-    console.log("You win best of 5!");
-    } else if (computerScore > playerScore){
-      console.log("Computer wins best of 5 :\(")
-    } else {console.log("It\'s a tie!")}
+  //if (playerScore > computerScore){
+    //console.log("You win best of 5!");
+    //} else if (computerScore > playerScore){
+      //console.log("Computer wins best of 5 :\(")
+    //} else {console.log("It\'s a tie!")}
   
-}
+//}
 
-// console.log(game())
+//console.log(game())
