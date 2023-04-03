5533e3206a1b384f10ad712921161a969bb5ce93
n-dett <124851780+n-dett@users.noreply.github.com>
2023-04-03 18:42:24 -0500

    Add function to disable game buttons
    Game buttons are disabled once playerScore or computerScore reaches 5

--- rock-p-s.js
+++ rock-p-s.js
@@ -6,37 +6,31 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-
-/////////////////////////
-
 const gameBtns = document.querySelectorAll('.gameBtns');
 
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
+let playerScore = 0;
+let computerScore = 0;
+
 // Play round with correct button id
 gameBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     console.log(btn.id);
     console.log(playRound(btn.id, getComputerChoice()));
+    })
   })
-})
+
+
 
 ////////////////////////
 
 // Play one round of rock, paper, scissors and show result
 function playRound(playerSelection, computerSelection){
-  let computerChoiceText = document.querySelector('#computer-choice-text');
-  let playerChoiceText = document.querySelector('#player-choice-text');
-  //let roundResultText = document.querySelector('#round-result-text');
 
-  let playerWins = false;
-  let computerWins = false;
-  let tieGame = false;
-
-  computerChoiceText.innerText = `Computer chose ${computerSelection}`;
-  playerChoiceText.innerText = `You chose ${playerSelection}`;
+  choicesText(playerSelection, computerSelection);
 
   if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'paper' && computerSelection === 'rock') ||
@@ -44,26 +38,37 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
     playerScore++;
     playerWinsText(playerSelection, computerSelection);
-    //roundResultText.innerText = `Point for you! ${playerSelection} beats ${computerSelection}.`
   }
   else if(playerSelection === computerSelection){
-    //roundResultText.innerText = 'It\'s a tie!'
     tieGameText();
   }
   else {
     computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1);
     computerScore++;
-    //roundResultText.innerText = `Point for computer. ${computerSelection} beats ${playerSelection}.`
     computerWinsText(playerSelection, computerSelection);
   }
 
   updateScoresText();
   gameResult();
+  gameIsOver();
 }
 
-let playerScore = 0;
-let computerScore = 0;
+function gameIsOver(){
+  gameBtns.forEach(btn => {
+  if(playerScore === 5 || computerScore === 5){
+    btn.disabled = true;
+  } 
+  })
+}
 
+
+
+function choicesText(playerSelection, computerSelection){
+  let computerChoiceText = document.querySelector('#computer-choice-text');
+  let playerChoiceText = document.querySelector('#player-choice-text');
+  computerChoiceText.innerText = `Computer chose ${computerSelection}`;
+  playerChoiceText.innerText = `You chose ${playerSelection}`;
+}
 
 function updateScoresText(){
   let playerScoreText = document.querySelector('#player-score-text');
@@ -86,23 +91,8 @@ function computerWinsText(playerSelection, computerSelection) {
   let roundResultText = document.querySelector('#round-result-text');
   roundResultText.innerText = `Point for computer. ${computerSelection} beats ${playerSelection}.`;
 }
-
-
-
-
-
-//function game(){
-  //for (let i = 0; i < 5; i++){
-    //let playerSelection = prompt("Enter rock, paper, or scissors").toLowerCase(); 
-    //const computerSelection = getComputerChoice();
-    //console.log(computerSelection);
-
-    //console.log(playRound(playerSelection, computerSelection));
-    //console.log('Your score: ' + playerScore) 
-    //console.log('Computer score: ' + computerScore)
-  //}
   
-  // Play until either player or computer reaches 5 points
+  // Declare a winner once player or computer reaches 5 points
   function gameResult(){
     let gameResultText = document.querySelector('#game-result-text');
     if (playerScore === 5){
@@ -111,8 +101,3 @@ function computerWinsText(playerSelection, computerSelection) {
         gameResultText.innerText = "Computer wins the game."
       }
   }
-//}
-
-//console.log(game())
-
-// Make smaller functions and call them inside playRound
